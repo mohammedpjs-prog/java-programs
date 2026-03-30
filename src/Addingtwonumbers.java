26008504ad13b1e2a3139ccc30553931a05c13f7
burhan <burhaxnu006@gmail.com>
2026-03-30 11:18:44 +0530

    sum of two numbers

---
+++ src/Addingtwonumbers.java
@@ -0,0 +1,8 @@
+public class Addingtwonumbers {
+    public static void main(String[] args){
+        int a = 4;
+        int b = 5;
+        int result = a +b;
+        System.out.println("the total sum is " + result);
+    }
+}
