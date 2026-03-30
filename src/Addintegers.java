2b47f77fb6c3d6850982d995d8568d88ee6d2d33
burhan <burhaxnu006@gmail.com>
2026-03-30 13:20:47 +0530

    Addintegers

---
+++ src/Addintegers.java
@@ -0,0 +1,14 @@
+ public class Addintegers {
+    public static void main(String[] args) {
+        // Declare two integer variables
+        int num1 = 10;
+        int num2 = 20;
+
+        // Perform addition
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}{
+}
