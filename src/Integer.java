ba3afa2dfd946423a382f00da15e32098a5923cb
burhan <burhaxnu006@gmail.com>
2026-03-30 12:31:48 +0530

    Interger

---
+++ src/Integer.java
@@ -0,0 +1,9 @@
+    public class Integer {
+      public static void main(String[] args) {
+        int age = 20;
+        String name = "burhan";
+
+        System.out.println("Age: " + age);
+        System.out.println("Name: " + name);
+    }
+}
