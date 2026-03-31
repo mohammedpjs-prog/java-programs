a39e0c9e263c4c333794243c8f4fbd41d07dadb1
burhan <burhaxnu006@gmail.com>
2026-03-31 16:34:41 +0530

    OperatorsExample

---
+++ src/OperatorsExample.java
@@ -0,0 +1,24 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class OperatorsExample {
+    public static void main(String[] args){
+
+    int a = 10;
+    int b = 20;
+
+
+    int sum = a + b;
+    int diff = a - b;
+    int product = a * b;
+    int div = a/b;
+    int rem = a % b;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(product);
+        System.out.println(div);
+        System.out.println(rem);
+
+    }
+
+
+}
