abdea15a9bd9efa7729a75cca1c914b1577574a8
burhan <burhaxnu006@gmail.com>
2026-04-12 11:32:47 +0530

    java class day 7

---
+++ src/program23.java
@@ -0,0 +1,13 @@
+public class program23 {
+    public static void main(String[] args){
+        int n = 3;
+        int sum = 0;
+        for (int i = 1; i<=n; i++){
+            sum+=i;
+        }
+        System.out.println(" sum = "+sum);
+
+        }
+
+    }
+
