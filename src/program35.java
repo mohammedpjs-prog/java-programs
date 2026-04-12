6cc2016c7462d88c0d077774f4756283f5785965
burhan <burhaxnu006@gmail.com>
2026-04-12 17:04:27 +0530

    java class day7

---
+++ src/program35.java
@@ -0,0 +1,13 @@
+public class program35 {
+    public static void triangle(int n){
+        for (int i = 1; i<=n; i++){
+            for (int j = 1; j<=i; j++){
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        triangle(5);
+    }
+}
