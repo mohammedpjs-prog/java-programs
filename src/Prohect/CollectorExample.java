ee8c1f5abcdce7a3dce7fc14f9ce138f131cb5fc
Your Name <youremail@example.com>
2026-05-26 15:39:43 +0530

    java class day 21

---
+++ src/Prohect/CollectorExample.java
@@ -0,0 +1,17 @@
+package Prohect;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class CollectorExample {
+    public static void main(String[] args){
+        List<Integer> numbers =
+                Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> evenumbers =
+                numbers.stream()
+                        .filter(n -> n % 2 == 0)
+                        .collect(Collectors.toList());
+        System.out.println(evenumbers);
+    }
+}
